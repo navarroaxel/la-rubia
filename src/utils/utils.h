65327ad97d4592a295ad44a70da3bdf2abada3ec
Axel Navarro <navarroaxel@users.noreply.github.com>
2011-09-11 23:23:55 +0000

    normalizacion de codigo

---
+++ src/utils/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include "logs.h"
+
+char* charmalloc(int len);
+
+char* substring(char* string, int begin, int end);
+
+void stringconcat(char* str1, char* str2);
+
+#endif
