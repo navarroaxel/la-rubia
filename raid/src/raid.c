908e2df14507ec7ee58b15af872548e9f219902f
Axel Navarro <navarroaxel@users.noreply.github.com>
2011-10-04 12:42:42 +0000

    socket connection sample

--- raid/src/raid.c
+++ raid/src/raid.c
@@ -14,8 +14,11 @@
 #include <assert.h>
 
 int main(void) {
+	t_socket_client *client = sockets_createClient("127.0.0.1", 5500);
+
+	sockets_connect(client, "127.0.0.1", 5200);
 	t_disk_readSectorRq *rq = malloc(sizeof(t_disk_readSectorRq));
-	rq->offset = 4;
+	rq->offset = 0;
 
 	t_nipc *nipc = nipc_create(NIPC_READSECTOR_RQ);
 	t_disk_readSectorRq *rq2;
@@ -23,6 +26,8 @@ int main(void) {
 	nipc_setdata(nipc,rq, sizeof(t_disk_readSectorRq));
 
 	t_socket_buffer *buffer = (t_socket_buffer *)nipc_serializer(nipc);
+	sockets_send(client, buffer->data, buffer->size);
+
 	nipc2 = (t_nipc *)nipc_deserializer(buffer);
 	rq2 = nipc2->payload;
 
