0f5362224c2596116e48bda68543bae6e2b21ac8
Axel Navarro <navarroaxel@users.noreply.github.com>
2011-10-05 00:18:09 +0000

    ejemplo de sockets

--- raid/src/raid.c
+++ raid/src/raid.c
@@ -14,26 +14,28 @@
 #include <assert.h>
 
 int main(void) {
-	t_socket_client *client = sockets_createClient("127.0.0.1", 5500);
+	t_socket_client *client = sockets_createClient("127.0.0.1", 5200);
 
 	sockets_connect(client, "127.0.0.1", 5200);
 	t_disk_readSectorRq *rq = malloc(sizeof(t_disk_readSectorRq));
 	rq->offset = 0;
 
 	t_nipc *nipc = nipc_create(NIPC_READSECTOR_RQ);
-	t_disk_readSectorRq *rq2;
+	t_disk_readSectorRs *rs;
 	t_nipc *nipc2;
-	nipc_setdata(nipc,rq, sizeof(t_disk_readSectorRq));
+	nipc_setdata(nipc, rq, sizeof(t_disk_readSectorRq));
 
 	t_socket_buffer *buffer = (t_socket_buffer *)nipc_serializer(nipc);
 	sockets_send(client, buffer->data, buffer->size);
 
-	nipc2 = (t_nipc *)nipc_deserializer(buffer);
-	rq2 = nipc2->payload;
+	buffer = sockets_recv(client);
+	nipc2 = nipc_deserializer(buffer);
+	rs = nipc2->payload;
 
-	assert(nipc->type == nipc2->type);
-	assert(nipc->length == nipc2->length);
-	assert(rq2->offset == rq->offset);
+	printf("%.*s\n", DISK_SECTOR_SIZE, rs->data);
+	printf("%1024X\n", rs->data);
+	printf("%i\n", rs->offset);
+	printf("%i\n", rs->result);
 
 	return EXIT_SUCCESS;
 }
