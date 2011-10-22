f031edb9530b6fd80da37dd343bccb814f5de44d
Nicolas Far <nicofff@users.noreply.github.com>
2011-10-22 02:55:55 +0000

    cambios script regeneracion volumen

--- build-utils/regenerarVolumen.sh
+++ build-utils/regenerarVolumen.sh
@@ -4,5 +4,5 @@ sudo mount -o loop fat32.disk fuseMount/
 cd fuseMount/
 #sudo nano asd.txt
 cd ..
-sudo cp Desktop/chatGaby.txt fuseMount/
-#sudo umount -f fuseMount
+echo "hello world!" > asd.txt
+sudo umount -f fuseMount
